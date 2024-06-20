671f1ca8bbd0b53ae11e5f73a973c396260bb528
boss225 <nguyendangvinh_92dn@yahoo.com>
2024-06-20 10:38:39 +0000

    test

--- pages/room/[id].js
+++ pages/room/[id].js
@@ -1,7 +1,7 @@
 import { useRouter } from "next/router";
 import { useEffect, useRef, useState } from "react";
 import { io } from "socket.io-client";
-import useSocket from "../hooks/useSocket";
+import useSocket from "../../hooks/useSocket";
 
 const ICE_SERVERS = {
   iceServers: [{ urls: "stun:openrelay.metered.ca:80" }],
